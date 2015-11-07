c14ef0060f920ac4a492c1ff8dd1fc36af0e27b1
Donato Galo <dgalo@ogangi.com>
2015-11-07 16:41:21 -0430

    Initial commit

---
+++ smstool/src/main/java/com/venbel/smstool/controller/ViewController.java
@@ -0,0 +1,35 @@
+package com.venbel.smstool.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ViewController {
+
+	@RequestMapping(value="/index")
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) //
+			throws ServletException, IOException {
+
+		return new ModelAndView("index");
+
+	}
+
+	@RequestMapping(value="/test/{page}")
+	public ModelAndView test(HttpServletRequest request, HttpServletResponse response, //
+			@PathVariable String page) throws ServletException, IOException {
+
+		System.out.println("Redirect to " + page + ".jsp");
+
+		return new ModelAndView(page);
+
+	}
+
+}
---
+++ smstool/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Systematic Measurement System (SMS) Tool</title>
+</head>
+<body>
+	<h1>Systematic Measurement System (SMS) Tool</h1>
+</body>
+</html>
